b1c3a98846c78da174e608ccfbe7bf5d5a3ae5cf
georgexiang <xiangzhihua@gmail.com>
2014-08-08 16:22:13 +0800

    bike single page application front project

---
+++ bikespa/app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name bikespaApp
+ * @description
+ * # bikespaApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('bikespaApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
