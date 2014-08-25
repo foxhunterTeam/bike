0535d2d836d520c197b714e4bebf4187859faaa2
georgexiang <xiangzhihua@gmail.com>
2014-08-25 13:40:05 +0800

    add bike register page

--- bikespa/app/scripts/controllers/gallery.js
+++ bikespa/app/scripts/controllers/gallery.js
@@ -23,6 +23,75 @@ angular.module('bikespaApp.gallery',['bikespaApp.grandfather'])
             });
     })
     .controller('GalleryCtrl', function ($scope, $http, $timeout, $state) {
+        /***
+         *
+         * init bikes
+         *
+         */
+        $scope.bikes=[
+            {
+                title: 'Bike1',
+                href: 'images/gallery/p1.jpg',
+                type: 'image/jpeg',
+                thumbnail: 'images/gallery/p1.jpg'
+            },
+            {
+                title: 'Bike2',
+                href: 'images/gallery/p2.jpg',
+                type: 'image/jpeg',
+                thumbnail: 'images/gallery/p2.jpg'
+            },
+            {
+                title: 'Bike3',
+                href: 'images/gallery/p3.jpg',
+                type: 'image/jpeg',
+                thumbnail: 'images/gallery/p3.jpg'
+            },
+            {
+                title: 'Bike4',
+                href: 'images/gallery/p4.jpg',
+                type: 'image/jpeg',
+                thumbnail: 'images/gallery/p4.jpg'
+            },
+            {
+                title: 'Bike5',
+                href: 'images/gallery/p5.jpg',
+                type: 'image/jpeg',
+                thumbnail: 'images/gallery/p5.jpg'
+            },
+            {
+                title: 'Bike6',
+                href: 'images/gallery/p6.jpg',
+                type: 'image/jpeg',
+                thumbnail: 'images/gallery/p6.jpg'
+            },
+            {
+                title: 'Bike7',
+                href: 'images/gallery/p7.jpg',
+                type: 'image/jpeg',
+                thumbnail: 'images/gallery/p7.jpg'
+            },
+            {
+                title: 'Bike8',
+                href: 'images/gallery/p8.jpg',
+                type: 'image/jpeg',
+                thumbnail: 'images/gallery/p8.jpg'
+            },
+            {
+                title: 'Bike9',
+                href: 'images/gallery/p9.jpg',
+                type: 'image/jpeg',
+                thumbnail: 'images/gallery/p9.jpg'
+            },
+            {
+                title: 'Apple',
+                href: 'http://example.org/images/apple.jpg',
+                type: 'image/jpeg',
+                thumbnail: 'http://example.org/thumbnails/apple.jpg'
+            }
+
+        ]
+
 
         /**
          *  init gallery click function
